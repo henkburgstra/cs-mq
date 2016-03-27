96bd3ec8680706eb199d2339baa56e857f837a21
Henk Burgstra <henk.burgstra@gmail.com>
2016-03-27 20:53:54 +0200

    Eerste commit

---
+++ CsMq/CsMq/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsMq
+{
+    [DataContract]
+    public struct Message
+    {
+        [DataMember]
+        public String Sender;
+
+        [DataMember]
+        public String Function;
+
+        [DataMember]
+        public Boolean Relay;
+
+        [DataMember]
+        public Boolean KeepAlive;
+    }
+
+    class Server
+    {
+    }
+}
