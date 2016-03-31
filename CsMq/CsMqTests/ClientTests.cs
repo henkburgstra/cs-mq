59ce05fd82c43906bd9906340c13a346fbfe1863
henk.burgstra@gmail.com <henk.burgstra@gmail.com>
2016-03-31 16:09:59 +0200

    MessageToJson plus unittest

--- CsMq/CsMqTests/ClientTests.cs
+++ CsMq/CsMqTests/ClientTests.cs
@@ -9,10 +9,10 @@ using System.Threading.Tasks;
 namespace CsMq.Tests
 {
     [TestClass()]
-    public class ServerTests
+    public class ClientTests
     {
         [TestMethod()]
-        public void MessageFromJsonTest()
+        public void MessageToJsonTest()
         {
             string testMsg = @"
             {
@@ -20,13 +20,17 @@ namespace CsMq.Tests
                 ""function"": ""function"",
                 ""relay"": false,
                 ""keep_alive"": true,
-                ""payload"": {}
+                ""payload"": {
+                    ""begindatum"": ""2016-03-31"",
+                    ""einddatum"": ""2016-03-13"",
+                    ""periode"": ""dagelijks"",
+                    ""timestamp"": ""2016-03-31 15:54:48"",
+                    ""uuid"": ""220e32cf-f748-11e5-914c-6c626d98ae88""
+                }
             }";
             Message message = Server.MessageFromJson(testMsg);
-            Assert.AreEqual(message.Sender, "sender");
-            Assert.AreEqual(message.Function, "function");
-            Assert.AreEqual(message.Relay, false);
-            Assert.AreEqual(message.KeepAlive, true);
+            string serializedMsg = Client.MessageToJson(message);
+            Assert.AreEqual(testMsg, serializedMsg);
         }
     }
 }
